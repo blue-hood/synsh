20bf5ba6a6b9378222aeb82fc83c0a327fe0eac8
hato6502 <erisuke12345@yahoo.co.jp>
2019-03-24 23:38:09 +0900

    Node.js でコマンドI/Oができました。

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 'use strict'
 
 const bufferSize = 1024;
+const sampleRate = 44100;
 
 const Speaker = require('speaker');
 const readline = require('readline');
+const synth = require('./synth');
 
 const buffer = new Buffer(bufferSize * 2);
 const speaker = new Speaker({
     channels: 1,
     bitDepth: 16,
-    sampleRate: 44100,
+    sampleRate: sampleRate,
 });
 
 const rl = readline.createInterface({
@@ -19,14 +21,42 @@ const rl = readline.createInterface({
     prompt: '> ',
 });
 
-rl.prompt();
-rl.on('line', (line) => {
-    console.log(line.split(/\s+/));
+synth.setOnCompleteHandler(() => {
     for (let i = 0; i < buffer.length; i += 2) {
-        buffer.writeInt16LE(Math.round(Math.sin(i / 10.0) * 32767.499999), i);
+        buffer.writeInt16LE(Math.round(Math.sin(2.0 * Math.PI * 440.0 * i / sampleRate) * 32767), i);
     }
     speaker.write(buffer);
     rl.prompt();
+});
+
+rl.prompt();
+rl.on('line', (line) => {
+    let args = line.replace(/#.*/, '').trim().split(/\s+/);
+    if (args[0] === '') args = [];
+
+    synth.pushOnSuccessHandler((response) => {
+        const printResponse = (response, level) => {
+            for (const key in response) {
+                const value = response[key];
+                let line = '';
+
+                for (let i = 0; i < level; i++) line += ' ';
+                line += `${key}: `;
+
+                if (value instanceof Object) {
+                    console.log(line);
+                    printResponse(value, level + 2);
+                } else {
+                    line += value;
+                    console.log(line);
+                }
+
+            }
+        };
+
+        printResponse(response, 0);
+    });
+    synth.write(args);
 }).on('close', () => {
     process.exit(0);
 });
---
+++ synth.js
@@ -0,0 +1,59 @@
+const { spawn } = require('child_process');
+const path = require('path');
+
+let receiveBuffer = "";
+let onSuccessHandlers = [];
+let onComplete = () => { };
+
+const synth = spawn('synth');
+synth.stdout.on('data', (data) => {
+    let dataString = data.toString();
+
+    while (dataString !== '') {
+        const index = dataString.indexOf("\0");
+
+        if (index != -1) {
+            receiveBuffer += dataString.slice(0, index);
+            dataString = dataString.slice(index + 1);
+            receive(JSON.parse(receiveBuffer).response);
+            receiveBuffer = '';
+        } else {
+            receiveBuffer += dataString;
+            dataString = '';
+        }
+    }
+});
+
+synth.stderr.on('data', (data) => {
+    console.log(data);
+});
+
+const write = (args) => {
+    const request = { request: { args: args } };
+    synth.stdin.write(JSON.stringify(request) + "\0");
+};
+
+const receive = (response) => {
+    const onSuccess = onSuccessHandlers.shift();
+
+    if ('error' in response) {
+        console.log(response.error);
+    } else {
+        onSuccess(response);
+    }
+    if (onSuccessHandlers.length === 0) onComplete();
+};
+
+const pushOnSuccessHandler = (handler) => {
+    onSuccessHandlers.push(handler);
+};
+
+const setOnCompleteHandler = (handler) => {
+    onComplete = handler;
+};
+
+module.exports = {
+    write: write,
+    pushOnSuccessHandler: pushOnSuccessHandler,
+    setOnCompleteHandler: setOnCompleteHandler,
+};
